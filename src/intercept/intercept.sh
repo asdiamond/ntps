12a356ff5967e5aaa67ac4bda33ef5f9b5314714
aleksandr <asdiamond@miners.utep.edu>
2019-05-14 16:31:46 -0600

    Commited a prototype interceptor with scripts that achieve iptables functionality. Needs to be wrapped in a usable api.

--- src/intercept/intercept.sh
+++ src/intercept/intercept.sh
@@ -1,5 +1,6 @@
-# retrieved from pypacker examples_interceptor.py
-iptables -I INPUT 1 -p icmp -j NFQUEUE --queue-balance 0:2
+# modified from pypacker and python-netfilterqueue, examples
+# only works on icmp packets right now
+iptables -I INPUT 1 -p icmp -j NFQUEUE --queue-num 1
 
 echo "NEW CONFIGURATION IS:"
 iptables -S
