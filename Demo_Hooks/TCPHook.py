80df50246cd7fccec43af48d85a3359c89bbde73
ingarcia1 <35977093+ingarcia1@users.noreply.github.com>
2019-05-15 21:18:44 -0600

    Add files via upload

--- Demo_Hooks/TCPHook.py
+++ Demo_Hooks/TCPHook.py
@@ -1,3 +1,11 @@
-#!/usr/bin/env python3
-
 from scapy.all import *
+
+def apply(pkt):
+	if(packet.haslayer(TCP)):
+		pkt[TCP].sport = 55555
+		del pkt.chksum
+		del pkt.len
+		del pkt[TCP].chksum
+		
+	pkt.show2(dump=True)
+	return pkt
