3d3d6ff246aaa26cd03f29a295735d9f5f137239
ingarcia1 <35977093+ingarcia1@users.noreply.github.com>
2019-05-15 21:26:30 -0600

    Add files via upload

--- Demo_Hooks/DNSHook.py
+++ Demo_Hooks/DNSHook.py
@@ -1,3 +1,11 @@
-#!/usr/bin/env python3
-
 from scapy.all import *
+
+def apply(pkt):
+	if(packet.haslayer(DNS)):
+		pkt[UDP].sport = 44444
+		del pkt.chksum
+		del pkt.len
+		del pkt[TCP].chksum
+		
+	pkt.show2(dump=True)
+	return pkt
